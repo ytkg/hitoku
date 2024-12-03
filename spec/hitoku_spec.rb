ef385e234483708dab2b11dfc52a4d174d676cbb
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2024-12-04 01:03:16 +0900

    Remove unnecessary tests

--- spec/hitoku_spec.rb
+++ spec/hitoku_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Hitoku do
   it 'has a version number' do
     expect(Hitoku::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
